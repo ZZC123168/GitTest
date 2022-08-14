001617e2b040134b678e551de460172f3e244a02
ZZC <978835752@qq.com>
2022-08-14 13:18:44 +0800

    第一次提交

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private string str = "First";
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
